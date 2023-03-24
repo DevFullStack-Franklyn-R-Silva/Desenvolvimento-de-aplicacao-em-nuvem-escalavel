0946244c7de4ee2ffd1f838f39fd4b66adf41194
hadesfranklyn <franklyn1998@live.com>
2023-03-24 20:41:13 -0300

    Consuming the Cambio Service Endpoint in the Book Service

--- book-service/src/main/java/com/github/hadesfranklyn/project/controller/BookController.java
+++ book-service/src/main/java/com/github/hadesfranklyn/project/controller/BookController.java
@@ -1,14 +1,18 @@
 package com.github.hadesfranklyn.project.controller;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.github.hadesfranklyn.project.model.Book;
 import com.github.hadesfranklyn.project.repository.BookRepository;
+import com.github.hadesfranklyn.project.response.Cambio;
 
 @RestController
 @RequestMapping("book-service")
@@ -20,17 +24,29 @@ public class BookController {
 	@Autowired
 	private Environment environment;
 
+	// http://localhost:8100/book-service/14/BRL
 	@GetMapping(value = "/{id}/{currency}")
 	public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
 
 		@SuppressWarnings("deprecation")
 		var book = repository.getById(id);
+
 		if (book == null)
 			throw new RuntimeException("Book not Found");
 
-		var port = environment.getProperty("local.server.port");
+		HashMap<String, String> params = new HashMap<>();
+		params.put("amount", book.getPrice().toString());
+		params.put("from", "USD");
+		params.put("to", currency);
 
+		var response = new RestTemplate().getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}",
+				Cambio.class, params);
+
+		var cambio = response.getBody(); // extract the exchange
+
+		var port = environment.getProperty("local.server.port");
 		book.setEnviroment(port);
+		book.setPrice(cambio.getConvertedValue());
 		return book;
 	}
 }
