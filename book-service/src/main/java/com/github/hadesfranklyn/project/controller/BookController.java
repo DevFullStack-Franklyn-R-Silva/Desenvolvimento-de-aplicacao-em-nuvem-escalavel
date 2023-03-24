c86d9691e61291d31dc2b2b16f1eb66d5d919603
hadesfranklyn <franklyn1998@live.com>
2023-03-24 20:46:47 -0300

    Using the Feign REST Client to Invoke the Cambio Service

--- book-service/src/main/java/com/github/hadesfranklyn/project/controller/BookController.java
+++ book-service/src/main/java/com/github/hadesfranklyn/project/controller/BookController.java
@@ -1,18 +1,15 @@
 package com.github.hadesfranklyn.project.controller;
 
-import java.util.HashMap;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.github.hadesfranklyn.project.model.Book;
+import com.github.hadesfranklyn.project.proxy.CambioProxy;
 import com.github.hadesfranklyn.project.repository.BookRepository;
-import com.github.hadesfranklyn.project.response.Cambio;
 
 @RestController
 @RequestMapping("book-service")
@@ -24,7 +21,9 @@ public class BookController {
 	@Autowired
 	private Environment environment;
 
-	// http://localhost:8100/book-service/14/BRL
+	@Autowired
+	private CambioProxy proxy;
+
 	@GetMapping(value = "/{id}/{currency}")
 	public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
 
@@ -34,19 +33,12 @@ public class BookController {
 		if (book == null)
 			throw new RuntimeException("Book not Found");
 
-		HashMap<String, String> params = new HashMap<>();
-		params.put("amount", book.getPrice().toString());
-		params.put("from", "USD");
-		params.put("to", currency);
-
-		var response = new RestTemplate().getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}",
-				Cambio.class, params);
-
-		var cambio = response.getBody(); // extract the exchange
+		var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
 
 		var port = environment.getProperty("local.server.port");
-		book.setEnviroment(port);
+		book.setEnviroment(port + " FEING");
 		book.setPrice(cambio.getConvertedValue());
 		return book;
 	}
+
 }
