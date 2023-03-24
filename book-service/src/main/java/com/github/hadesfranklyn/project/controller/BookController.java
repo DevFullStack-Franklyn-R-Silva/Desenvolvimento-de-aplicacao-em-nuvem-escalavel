cfecf40a3337268be669aac652e7bf407b8c3d5c
hadesfranklyn <franklyn1998@live.com>
2023-03-24 20:35:45 -0300

    Fixing Problems with Jackson and Running

--- book-service/src/main/java/com/github/hadesfranklyn/project/controller/BookController.java
+++ book-service/src/main/java/com/github/hadesfranklyn/project/controller/BookController.java
@@ -1,7 +1,5 @@
 package com.github.hadesfranklyn.project.controller;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,31 +8,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.hadesfranklyn.project.model.Book;
+import com.github.hadesfranklyn.project.repository.BookRepository;
 
 @RestController
 @RequestMapping("book-service")
 public class BookController {
 
-
+	@Autowired
+	private BookRepository repository;
 
 	@Autowired
 	private Environment environment;
 
-	
-
 	@GetMapping(value = "/{id}/{currency}")
-	public Book findBook(
-			@PathVariable("id") Long id, 
-			@PathVariable("currency") String currency
-			) {
+	public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
 
-
-		
+		@SuppressWarnings("deprecation")
+		var book = repository.getById(id);
+		if (book == null)
+			throw new RuntimeException("Book not Found");
 
 		var port = environment.getProperty("local.server.port");
-		
 
-		return new Book(1L, "Nigel", "Docker", new Date(),
-				Double.valueOf(13.8), currency, port);
+		book.setEnviroment(port);
+		return book;
 	}
 }
