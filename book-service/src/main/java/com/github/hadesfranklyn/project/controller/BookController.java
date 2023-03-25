40e38b40b945185f8cff4a220471cabc48ed6ffe
hadesfranklyn <franklyn1998@live.com>
2023-03-24 21:11:34 -0300

    Implementing Load Balancing with Eureka, Feign, and Spring Cloud LoadBalancer

--- book-service/src/main/java/com/github/hadesfranklyn/project/controller/BookController.java
+++ book-service/src/main/java/com/github/hadesfranklyn/project/controller/BookController.java
@@ -36,9 +36,10 @@ public class BookController {
 		var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
 
 		var port = environment.getProperty("local.server.port");
-		book.setEnviroment(port + " FEING");
+		book.setEnviroment(
+				"Book port: "+ port + 
+				" Cambio port: " + cambio.getEnvironment());
 		book.setPrice(cambio.getConvertedValue());
 		return book;
 	}
-
 }
