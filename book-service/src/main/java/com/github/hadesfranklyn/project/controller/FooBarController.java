a4406a461c47c3d8f8f802f745b009d0435ed1d9
hadesfranklyn <franklyn1998@live.com>
2023-03-24 22:00:50 -0300

    Knowing and applying the Rate Limit and BulkHead Features in Resilien

--- book-service/src/main/java/com/github/hadesfranklyn/project/controller/FooBarController.java
+++ book-service/src/main/java/com/github/hadesfranklyn/project/controller/FooBarController.java
@@ -5,9 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 
 @RestController
 @RequestMapping("book-service")
@@ -16,12 +15,10 @@ public class FooBarController {
 	private Logger logger =LoggerFactory.getLogger(FooBarController.class);
 	
 	@GetMapping("/foo-bar")
-//	@Retry(name= "foo-bar", fallbackMethod = "fallbackMethod")
-	@CircuitBreaker(name= "default", fallbackMethod = "fallbackMethod")
+	@Bulkhead(name = "default")
 	public String fooBar() {
 		logger.info("Request to foo-bar is received!");
-		var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);		
-		return response.getBody();
+		return "Foo-Bar!!!";
 	}
 	public String fallbackMethod(Exception ex) {
 		return "fallbackMethod foo-bar!!!";
