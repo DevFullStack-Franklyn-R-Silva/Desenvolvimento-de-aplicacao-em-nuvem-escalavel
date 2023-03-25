0db03be65b64e3f5eb6381195743180035a9961f
hadesfranklyn <franklyn1998@live.com>
2023-03-24 21:16:33 -0300

    Spring Cloud API Gateway

--- api-gateway/src/main/java/com/github/hadesfranklyn/project/Application.java
+++ api-gateway/src/main/java/com/github/hadesfranklyn/project/Application.java
@@ -2,14 +2,12 @@ package com.github.hadesfranklyn.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class NamingServerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NamingServerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
