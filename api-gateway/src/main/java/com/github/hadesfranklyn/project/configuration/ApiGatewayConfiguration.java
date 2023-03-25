0af7bcdf20a7135a2813b856dc7003f83eed85a8
hadesfranklyn <franklyn1998@live.com>
2023-03-24 21:36:51 -0300

    Ajust errors

--- api-gateway/src/main/java/com/github/hadesfranklyn/project/configuration/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/github/hadesfranklyn/project/configuration/ApiGatewayConfiguration.java
@@ -15,12 +15,15 @@ public class ApiGatewayConfiguration {
 
 	@Bean
 	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
-		Function<PredicateSpec, Buildable<Route>> function = 
-				p -> p.path("/get")
-					.filters(f -> f
-							.addRequestHeader("Hello", "World")
-							.addRequestParameter("Hello", "World"))
-					.uri("http://httpbin.org:80");
-		return builder.routes().route(function).build();
+		Function<PredicateSpec, Buildable<Route>> function = p -> p.path("/get")
+				.filters(f -> f.addRequestHeader("Hello", "World").addRequestParameter("Hello", "World"))
+				.uri("http://httpbin.org:80");
+		return builder.routes()
+				.route(p -> p.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World").addRequestParameter("Hello", "World"))
+						.uri("http://httpbin.org:80"))
+				.route(p -> p.path("/cambio-service/**").uri("lb://cambio-service"))
+				.route(p -> p.path("/book-service/**").uri("lb://book-service")).build();
 	}
+
 }
