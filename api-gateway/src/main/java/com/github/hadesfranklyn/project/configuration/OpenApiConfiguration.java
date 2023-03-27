dd86a3f9067282a0467ab6493c4ca68f41a8b5c4
hadesfranklyn <franklyn1998@live.com>
2023-03-27 16:01:56 -0300

    Connecting Book Microservice to Zipkin with Zipkin and Sleuth

--- api-gateway/src/main/java/com/github/hadesfranklyn/project/configuration/OpenApiConfiguration.java
+++ api-gateway/src/main/java/com/github/hadesfranklyn/project/configuration/OpenApiConfiguration.java
@@ -6,11 +6,15 @@ import java.util.List;
 import org.springdoc.core.GroupedOpenApi;
 import org.springdoc.core.SwaggerUiConfigParameters;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class OpenApiConfiguration {
 
+	@Bean
+	@Lazy(false)
 	public List<GroupedOpenApi> apis(SwaggerUiConfigParameters config,
 			RouteDefinitionLocator locator) {
 
